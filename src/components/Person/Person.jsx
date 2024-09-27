3923b583fabd9b35ca3927d3d847d0f513fab79d
Vitaliy <vitaliklevytskyi07@gmail.com>
2024-09-27 20:39:19 +0300

    add task solution 2.0

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -7,7 +7,9 @@ export default function Person({ person }) {
   return (
     <section className="Person">
       <h2 className="Person__name">My name is {person.name}</h2>
+
       {person.age && <p className="Person__age">I am {person.age}</p>}
+
       <p className="Person__partner">{personPartner}</p>
     </section>
   );
